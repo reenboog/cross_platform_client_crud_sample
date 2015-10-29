db4b33cbc192f6d9be6a9590705ec15ecc06cf0c
reenboog <alex.gievsky@gmail.com>
2015-10-29 23:21:59 +0600

    Updated mac ServerAPI.

--- proj.ios_mac/mac/ServerAPI.mm
+++ proj.ios_mac/mac/ServerAPI.mm
@@ -196,7 +196,7 @@ void ServerAPI::updateGoalForUser(const string &userId, int calories, OnGoalUpda
                     NSString *errorString = [error userInfo][@"error"];
                     
                     string errorStr = errorString ? [errorString UTF8String] : "Error loading in.";
-
+                    
                     failedToUpdateGoalCallback("error", errorStr);
                 }
             }];
@@ -216,15 +216,43 @@ void ServerAPI::createMeal(const string &caption, int calories, OnMealCreatedCal
         ServerAPI::sharedInstance();
     }
     
+    //
+    
     // start parse stuff here
+    
+    int consumedAtTime = Date::time();
+    Date consumedAtDate = Date::now();
+    
+    // prepare NSDate
+    //NSString *currentStrDate = @"2008/08/18 13-45-01";
+    NSString *currentStrDate = [NSString stringWithFormat: @"%i/%i/%i", consumedAtDate.getYear(), consumedAtDate.getMonth(), consumedAtDate.getDay()];
+    NSDateFormatter *dateFormatterStr = [[NSDateFormatter new] autorelease];
+    
+    [dateFormatterStr setTimeZone: [NSTimeZone timeZoneForSecondsFromGMT: 0]];
+    [dateFormatterStr setDateFormat: @"yyyy/MM/dd"];
+    
+    NSDate *dateFromStr = [dateFormatterStr dateFromString: currentStrDate];
+    
+    //
+    
     PFObject *meal = [PFObject objectWithClassName: @"Meal"];
     [meal setObject: [NSString stringWithUTF8String: caption.c_str()] forKey: @"caption"];
-    [meal setObject: [PFUser currentUser] forKey: @"consumedBy"];
+    [meal setObject: [NSString stringWithUTF8String: User::sharedInstance()->getId().c_str()] forKey: @"consumedBy"];
     [meal setObject: [NSNumber numberWithInt: calories] forKey: @"calories"];
+    [meal setObject: [NSNumber numberWithInt: consumedAtTime] forKey: @"consumedAtTime"];
+    [meal setObject: dateFromStr forKey: @"consumedAtDate"];
+    
     
     [meal saveInBackgroundWithBlock:^(BOOL succeeded, NSError *error) {
+        // Kick off the new query, refresh the table, and then - once that's all
+        // taken care of - re-enable the item entry UI.
         if(!error) {
-            createdCallback();
+            //Meal(const Date &date, unsigned int time, const std::string &caption, const std::string userId, unsigned int calories, const std::string mealId);
+            string mealId = meal.objectId ? [meal.objectId UTF8String] : "unknown";
+            
+            Meal newMeal(consumedAtDate, consumedAtTime, caption, User::sharedInstance()->getId(), calories, mealId);
+            
+            createdCallback(newMeal);
         } else {
             NSString *errorString = [error userInfo][@"error"];
             
@@ -233,5 +261,49 @@ void ServerAPI::createMeal(const string &caption, int calories, OnMealCreatedCal
             failedToCreateCallback("error", errorStr);
         }
         
+    }];
+}
+
+void ServerAPI::fetchAvailableMeals(OnMealsFetchedCallback mealsFetchedCallback, onFailedToFetchMealsCallback mealsFetchFailedCallback) {
+    if(__sharedInstance == nullptr) {
+        ServerAPI::sharedInstance();
+    }
+    
+    PFQuery *query = [PFQuery queryWithClassName: @"Meal"];
+    [query whereKey: @"consumedBy" equalTo: [NSString stringWithUTF8String: User::sharedInstance()->getId().c_str()]];
+    [query orderByAscending: @"consumedAtDate"];
+    [query addAscendingOrder: @"consumedAtTime"];
+    [query findObjectsInBackgroundWithBlock:^(NSArray *objects, NSError *error) {
+        if(!error) {
+            for(PFObject *object in objects) {
+                int time = object[@"consumedAtTime"] ? [object[@"consumedAtTime"] intValue] : 0;
+                string caption = object[@"caption"] ? [object[@"caption"] UTF8String] : "undefined";
+                string userId = object[@"consumedBy"] ? [object[@"consumedBy"] UTF8String] : "user x";
+                string mealId = object.objectId ? [object.objectId UTF8String] : "unknown id";
+                int calories = object[@"calories"] ? [object[@"calories"] intValue] : 0;
+                
+                NSDate *date = object[@"consumedAtDate"] ? object[@"consumedAtDate"] : [NSDate date];
+                
+                NSDateComponents *components = [[NSCalendar currentCalendar] components: NSCalendarUnitDay | NSCalendarUnitMonth | NSCalendarUnitYear
+                                                                               fromDate: date];
+                
+                int day = [components day];
+                int month = [components month];
+                int year = [components year];
+                
+                // Meal(const Date &date, unsigned int time, const std::string &caption, const std::string userId, unsigned int calories, const std::string mealId);
+                Meal meal(Date(year, month, day), time, caption, userId, calories, mealId);
+                
+                User::sharedInstance()->addMeal(meal);
+            }
+            
+            mealsFetchedCallback(User::sharedInstance()->getAllMeal());
+        } else {
+            NSString *errorString = [error userInfo][@"error"];
+            
+            string errorStr = errorString ? [errorString UTF8String] : "Error fetching meals.";
+            
+            mealsFetchFailedCallback("error", errorStr);
+        }
     }];
 }
